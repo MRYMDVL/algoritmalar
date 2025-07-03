79f479053adda844ece4e25240895985185d9c58
bunyaminpilsim <bunyaminpilsim@gmail.com>
2025-07-03 22:27:48 +0300

    Verilen bir karakter dizisini (string) tersine çeviren algoritma

--- algoritmalar/Program.cs
+++ algoritmalar/Program.cs
@@ -1,3 +1,23 @@
 ﻿/*
  Verilen bir karakter dizisini (string) tersine çeviren algoritma
+        1-Başla
+        2-Bir metin giriniz.
+        3-Metnin uzunluğunu aldım.
+        4-Tersçevrilmismetin diye değişken atadım.
+        5-Döngüyü tersten başlat.
+        5.1-Ters çevrilmişmetine dizinin karakterlerini tersten ata.
+        6-Ters çevrilmiş metini ekrana yazdır.
+        7-Bitir
  */
+
+Console.WriteLine("Bir metin giriniz:");
+string metin = Console.ReadLine();
+int metinUzunlugu = metin.Length;
+string tersmetin = "";
+
+for (int i = metinUzunlugu - 1; i >= 0; i--)
+{
+    tersmetin = tersmetin + metin[i];
+
+}
+Console.WriteLine("Ters metin : " + tersmetin);
