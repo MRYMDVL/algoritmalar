393fadb285f2f570cd197e3bfa553936b98a4f1f
bunyaminpilsim <bunyaminpilsim@gmail.com>
2025-07-03 22:36:46 +0300

    Yeni soru eklendi

---
+++ enUzunKelimeyiBulmaAlgoritmasi/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace enUzunKelimeyiBulmaAlgoritmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Verilen metindeki en uzun kelimeyi bulan algoritma
+
+             */
+        }
+    }
+}
