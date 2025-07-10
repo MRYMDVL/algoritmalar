dc1d679aad3e69cdaf8eb5688587877406f07275
fatihmehmet <fatihmehmetguclugil@gmail.com>
2025-07-10 21:50:37 +0300

    Kullanıcıdan sürekli olarak sayı girmesini isteyin. Kullanıcı negatif bir sayı girdiğinde, döngüyü sonlandırın (break kullanarak). Girilen tek sayıları atlayın ve sadece çift sayıları ekrana yazdırın (continue kullanarak)

---
+++ tekSayılarıAtlayıpÇiftSayılarıYazdıranAlgoritma/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace tekSayılarıAtlayıpÇiftSayılarıYazdıranAlgoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Kullanıcıdan sürekli olarak sayı girmesini isteyin. Kullanıcı negatif bir sayı girdiğinde,
+            döngüyü sonlandırın (break kullanarak). Girilen tek sayıları atlayın ve sadece çift sayıları ekrana yazdırın
+            (continue kullanarak).
+            1.başla
+            2.sonsuz bir döngü başlat
+            3.kullanıcıdan bir sayi iste
+            4.eğer negatif ise bitir
+            5.eğer tek ise continou ile bu turu atla
+            6.eğer çift ise yazdır
+            7.bitir             
+            */
+            while (true)
+            {
+                Console.Write("Bir sayı girin: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi < 0)
+                {
+                    Console.WriteLine("Negatif sayı girildi.");
+                    break;
+                }
+                if (sayi % 2 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("Çift sayı: " + sayi);
+            }
+        }
+    }
+}
