e6d89b7e7a756ff17802a636a574adc442a00ebd
35aliakpnar <35akpnar@gmail.com>
2025-07-17 23:12:03 +0300

    Dijital saat fonksiyonu

---
+++ dijitalsaatFonksiyonu/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace dijitalsaatFonksiyonu
+{
+    internal class Program
+    {
+        static string SaniyeyiFormataCevir(int saniye)
+        {
+            int saat = saniye / 3600;
+            int dakika = (saniye % 3600) / 60;
+            int kalanSaniye = saniye % 60;
+
+            return $"{saat:D2}:{dakika:D2}:{kalanSaniye:D2}";
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Saniye cinsinden bir süre girin: ");
+            int tSaniye = int.Parse(Console.ReadLine());
+
+            string dijitalSaat = SaniyeyiFormataCevir(tSaniye);
+            Console.WriteLine("Dijital saat formatı: " + dijitalSaat);
+        }
+    }
+}
