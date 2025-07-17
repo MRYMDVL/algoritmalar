d47d9507bd003b0a93e2d90975b436d740b83819
MeryemDeveli <develi.meryemd@gmail.com>
2025-07-17 21:52:14 +0300

    selam ver fonksiyonu

---
+++ selamlaFonksiyonu/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace selamlaFonksiyonu
+{
+    internal class Program
+    {
+        public static void SelamVer(string isim, string saat)
+        {
+            Console.WriteLine($"merhaba, {isim},{saat} itibari ile sistem başlatılmıştır");
+        }
+        static void Main(string[] args)
+        {
+            SelamVer("meryem", "13.00");
+        }
+    }
+}
