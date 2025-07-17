3c328e51c8517012fddaa2d2c38db45aed2a08ff
Beyza Kırımtay <beyza.kirimtay@std.yeditepe.edu.tr>
2025-07-17 22:47:40 +0300

    toplam kargo ücreti hesaplayan fonksiyon

---
+++ kargoUcretiHesaplamaFonksiyonu/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace kargoUcretiHesaplamaFonksiyonu
+{
+    internal class Program
+
+    {
+        public static double ToplamKargoUcreti(double mesafe, double agirlik)
+        {
+            double mesafeUcreti = 25.78;
+            double agirlikUcreti = 35.89;
+            double defaultUcret = 100;
+            double toplamUcret = (mesafeUcreti * mesafe) + (agirlikUcreti * agirlik) + defaultUcret;
+            return toplamUcret;
+
+        }
+        static void Main(string[] args)
+        {
+            double kargoUcreti = ToplamKargoUcreti(100,45);
+            Console.WriteLine($"toplam kargo ücretiniz {kargoUcreti} tl dir");
+
+        }
+    }
+}
