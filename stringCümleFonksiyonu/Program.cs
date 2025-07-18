d833f6bcda8b2f3d18733899eddeab6bb228b332
fatihmehmet <fatihmehmetguclugil@gmail.com>
2025-07-18 21:52:34 +0300

    String fonksiyonları kullanarak bir cümledeki kelime sayısını bulan method.

---
+++ stringCümleFonksiyonu/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace stringCümleFonksiyonu
+{
+    internal class Program
+    {
+        public static int KelimeSayisi(string cumle)
+        {
+            //cumle = cumle.Trim(); başınnda ve sonundaki boşlukları yok saymamıza yarıyor
+            string[] kelimeler = cumle.Split(' ',StringSplitOptions.RemoveEmptyEntries); // tüm boşlukları yok saymamıza yarıyor
+            return kelimeler.Length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz: ");
+            string metin = Console.ReadLine();
+
+            int kelimeSayisi = KelimeSayisi(metin);
+            Console.WriteLine($"Metinde toplam {kelimeSayisi} kelime var.");
+        }
+    }
+}
