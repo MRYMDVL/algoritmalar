67ab9ed89f1e2ca8d7ee7483fd44625b9f0d1551
Beyza Kırımtay <beyza.kirimtay@std.yeditepe.edu.tr>
2025-07-18 22:22:10 +0300

    eposta adresini geçerliliğini kontrol eden fonksiyon

---
+++ epostaFormatiKontrolAlgoritmasi/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace epostaFormatiKontrolAlgoritmasi
+{
+    internal class Program
+    {
+        static bool EpostaKontrol(string eposta)
+        {
+            return eposta.Contains("@") && eposta.EndsWith(".com");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("E-posta adresini gir:");
+            string eposta = Console.ReadLine();
+            if (EpostaKontrol(eposta))
+            {
+                Console.WriteLine($"Gecerli eposta.epostaniz : {eposta}");
+
+            }
+            else
+            {
+                Console.WriteLine("Gcersiz eposta.");
+
+            }
+
+        }
+    }
+}
