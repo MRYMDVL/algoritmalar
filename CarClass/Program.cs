53e325dad2459fa53f6fd9635a58e26f9c2651b3
yusufsw <yusuf-talha@outlook.com>
2025-07-26 21:38:49 +0300

    Sınıf ve Capsulatıon ornegı

---
+++ CarClass/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CarClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", 2023);
+            car1.StepGas(100);
+            car1.StepBrake(50);
+            car1.StepBrake(500);
+
+            
+        }
+    }
+    class Car
+    {
+        
+        public string Brand;
+        public int Year;
+        private int Speed;
+        
+        public Car(string Brand, int Year) 
+        {
+            this.Brand = Brand;
+            this.Year = Year;
+        }
+        public void StepGas(int gas)
+        {
+            this.Speed += gas;
+            Console.WriteLine($"Current speed is {this.Speed}");
+        }
+        public void StepBrake(int brake)
+        {
+            if(Speed -  brake < 0)
+            {
+                Speed = 0;
+                Console.WriteLine($"Current speed is {Speed}");
+            }
+            else
+            {
+                Speed -= brake;
+                Console.WriteLine($"Current speed is {Speed}");
+            }
+        }
+        
+    }
+}
